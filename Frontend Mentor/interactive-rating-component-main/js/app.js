d7f51c3a4cce2237a993626da911648ea1377177
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-30 12:52:25 -0300

    selecionar e decelecionar os ratings

---
+++ Frontend Mentor/interactive-rating-component-main/js/app.js
@@ -0,0 +1,39 @@
+
+
+const rateEl = document.querySelectorAll("#rating");
+const btn = document.querySelector('#btn');
+const card = document.querySelector('.card');
+const cardRated = document.querySelector(".card-rated");
+
+
+
+getRate();
+
+function getRate() {
+	rateEl.forEach((rate) => {
+		rate.addEventListener('click', () => {
+			const rateValue = rate.getAttribute('data-value');
+
+			if (rateValue) {
+				rate.style.background = " hsl(25, 97%, 53%)";
+        		rate.style.color = "white";
+			}
+			
+			console.log(rateValue);
+
+			
+			btn.addEventListener("click", (e) => {
+			e.preventDefault();
+
+			card.classList.add("card-rated");
+
+			cardRated.style.display = "block";
+			});
+		});
+	});
+}
+
+
+
+
+
