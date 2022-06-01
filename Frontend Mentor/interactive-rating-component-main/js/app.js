c3c35a701ec92bd1ad689210bffe4a5329af8e97
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-06-01 12:42:38 -0300

    🤬

--- Frontend Mentor/interactive-rating-component-main/js/app.js
+++ Frontend Mentor/interactive-rating-component-main/js/app.js
@@ -7,59 +7,6 @@ const card = document.querySelector('.card');
 const cardRated = document.querySelector(".card-rated");
 const ratingPicked = document.querySelectorAll(".rating");
 
-
-
-getRate();
-
-
-
-
-
-function getRate() {
-	
-	rateEl.forEach((rate) => {
-
-		botaCor();
-		rate.addEventListener('click', () => {
-			const rateValue = rate.value;
-			let getValuerate = rateValue;
-			
-
-			console.log(rateValue,getValuerate);
-			
-		
-		});
-
-	});
-}
-console.log(getValuerate);
-
-
-submitBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-
-	card.classList.add("card-rated");
-
-	cardRated.style.display = "block";
-	
-
-});
-
-
-
-function botaCor() {
-  ratingPicked.forEach((butao) => {
-	  butao.addEventListener("click", () => {
-			butao.style.backgroundColor = "hsl(25, 97%, 53%)";
-     		butao.style.color = "white";
-		  
-		
-     
-    });
-  });
-}
-
-
-
+let selected = 0
 
 
