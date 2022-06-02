12db3078f2a5d6cd1c783b84c168fea515739e77
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-06-02 19:38:52 -0300

    nao fiz sozinho

--- Frontend Mentor/interactive-rating-component-main/js/app.js
+++ Frontend Mentor/interactive-rating-component-main/js/app.js
@@ -1,12 +1,31 @@
 
 
-const rateEl = document.querySelectorAll(".ratings");
-
-const submitBtn = document.querySelector("#Submit-btn");
 const card = document.querySelector('.card');
 const cardRated = document.querySelector(".card-rated");
 const ratingPicked = document.querySelectorAll(".rating");
+const submitBtn = document.querySelector("#Submit-btn");
+const rateValue = document.querySelector("#rate-selected");
 
-let selected = 0
+ratingPicked.forEach((rate, i) => {
+  rate.onclick = function () {
+    let currentRate = i + 1;
+	  ratingPicked.forEach((rate, j) => {
+      if (currentRate >= j + 1) {
+			rate.style.background = "hsl(25, 97%, 53%)";
+			rate.style.color = "white"
+		} else {
+			rate.style.background = "hsl(213, 19%, 18%)";
+			rate.style.color = "white"
+		}
+		rateValue.innerHTML = `${currentRate}`
+
+	 });
+	  submitBtn.addEventListener("click", () => {
+      cardRated.style.display = "block";
+      card.style.display = "none";
+    });
+  };
+});
+
 
 
