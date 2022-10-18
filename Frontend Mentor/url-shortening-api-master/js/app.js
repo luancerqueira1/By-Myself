7002d3ba210b030f5fefa0374f5fdfd0c6d14a46
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-10-18 19:44:59 -0300

    displayresults + input error added

--- Frontend Mentor/url-shortening-api-master/js/app.js
+++ Frontend Mentor/url-shortening-api-master/js/app.js
@@ -6,7 +6,7 @@ const apiResponse = async (URL) => {
       const respData = await resp.json();
 
       console.log(respData);
-      
+
       displayResult(respData.result.original_link, respData.result.short_link);
    } catch (err) {
       // showError();
@@ -14,7 +14,7 @@ const apiResponse = async (URL) => {
    }
 };
 
-/*====== GET LINK =====*/ 
+/*====== GET LINK =====*/
 
 getLink();
 
@@ -24,37 +24,53 @@ function getLink() {
 
    submitBtn.addEventListener('click', function (e) {
       e.preventDefault();
-      
-      const inputURL = input.value
+      if (!input.value) {
+         showInputError();
+      }
+
+      const inputURL = input.value;
       const fullURL = `https://api.shrtco.de/v2/shorten?url=${inputURL}`;
       apiResponse(fullURL);
+
+      input.value = '';
    });
 }
 
-
-/*====== SHOW RESULTS =====*/ 
-function displayResult(toShort,shortened) {
+/*====== SHOW RESULTS & COPY =====*/
+function displayResult(toShort, shortened) {
    const linkToShort = document.querySelector('.link-to-shorten');
    const shortResult = document.querySelector('.shortened-result');
    const shortContainer = document.querySelector('.short-result');
-
 
    linkToShort.innerHTML = `<p>${toShort}</p>`;
    shortResult.setAttribute('href', `https://${shortened}`);
    shortResult.textContent = shortened;
    shortContainer.classList.add('active');
 
-};
+   //COPY
+   const copyBtn = document.querySelector('.copy-btn');
 
+   copyBtn.addEventListener('click', function () {
+      navigator.clipboard.writeText(shortened);
 
+      copyBtn.classList.add('copied');
+      copyBtn.textContent = 'copied!'
+   });
+}
 
+/*===== INPUT ERROR =====*/
+function showInputError() {
+   const msgErr = document.querySelector('.error');
+   const inputErr = document.querySelector('#input-link');
 
+   inputErr.classList.add('error-input');
+   msgErr.classList.add('show-error');
 
-
-
-
-
-
+   setTimeout(function () {
+      inputErr.classList.remove('error-input');
+      msgErr.classList.remove('show-error');
+   }, 3000);
+}
 
 
 const nav = document.querySelector('.nav-links');
